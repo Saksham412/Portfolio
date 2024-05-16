6c6ec0d707ecc4e35232e487e28be10105fa9f04
Saksham <sakshamshinde412@gmail.com>
2024-05-16 14:12:58 +0530

    link update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
           <a href="https://www.linkedin.com/in/sakshamshinde12/" target="_blank"><FaLinkedinIn/></a>
-          <a href="https://www.instagram.com/saksham_shinde_12/" target="_blank"><FaInstagram/></a>
+          <a href="https://www.instagram.com/saksham_s_36/" target="_blank"><FaInstagram/></a>
           <a href="https://github.com/Saksham412" target="_blank"><FaGithub/></a>
           <a href="https://twitter.com/Saksham_412" target="_blank"><FaTwitter/></a>
         </div>
